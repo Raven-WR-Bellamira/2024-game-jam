e14c5c4e0bf7db4754c630af7199535db6705b68
Harvey <harvey2.Fettis@live.uwe.ac.uk>
2024-01-26 23:16:07 +0000

    Adding Scripts

---
+++ the game/Assets/Scripts/Card_Generation.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Card_Generation : MonoBehaviour
+{
+    //Script used to generate random cards
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
